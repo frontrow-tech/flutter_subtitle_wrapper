917c4adc4f5e9bd37fd275cf5965cf4a8ae85940
Ayush Shekhar <ayush@frontrow.co.in>
2020-09-05 18:45:08 +0530

    Created a copyWith method for subtitle position"

--- lib/data/models/style/subtitle_position.dart
+++ lib/data/models/style/subtitle_position.dart
@@ -6,4 +6,34 @@ class SubtitlePosition {
 
   const SubtitlePosition(
       {this.left = 0, this.right = 0, this.top, this.bottom = 50});
+
+  SubtitlePosition copyWith({
+    double left,
+    double right,
+    double top,
+    double bottom,
+  }) {
+    return SubtitlePosition(
+      left: left ?? this.left,
+      right: right ?? this.right,
+      top: top ?? this.top,
+      bottom: bottom ?? this.bottom,
+    );
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is SubtitlePosition &&
+        o.left == left &&
+        o.right == right &&
+        o.top == top &&
+        o.bottom == bottom;
+  }
+
+  @override
+  int get hashCode {
+    return left.hashCode ^ right.hashCode ^ top.hashCode ^ bottom.hashCode;
+  }
 }
